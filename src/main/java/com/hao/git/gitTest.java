ce46eee9658e20ea1d64dd15b34ae0f04f996614
xingkonghao <1105211547@qq.com>
2022-04-28 12:22:59 +0800

    second commit

--- src/main/java/com/hao/git/gitTest.java
+++ src/main/java/com/hao/git/gitTest.java
@@ -7,5 +7,6 @@ package com.hao.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
