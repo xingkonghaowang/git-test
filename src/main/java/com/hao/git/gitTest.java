4c54b183868a9e39d4f55887bf06b77c24bcd88c
xingkonghao <1105211547@qq.com>
2022-04-28 12:21:13 +0800

    first commit

---
+++ src/main/java/com/hao/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.hao.git;
+
+/**
+ * @author chenhao
+ * @date 2022/4/28 - 12:02
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
